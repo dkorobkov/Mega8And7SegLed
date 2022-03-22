2b9f6ee0dbb9090621ae341ae07dfad96aef4680
dkorobkov <devnull@mail.com>
2022-03-22 13:44:17 +0300

    Fixing repo

--- Src/Universal_LED_display_board/Universal_LED_display_board/Indicator.h
+++ Src/Universal_LED_display_board/Universal_LED_display_board/Indicator.h
@@ -11,7 +11,7 @@
  Current flows from ATMega pin through LED to another pin(s).
  +-----+-------+-----------+------------+
  | LED | Descr |  MCU pin  | Level for  |
- | pin |       |           | lighting   |
+ | pin |       | (ATMega8) | lighting   |
  +-----+-------+-----------+------------+
  | 1   |  e    |  PD4   2  |     1      |
  | 2   |  d    |  PC3   26 |     1      |
@@ -27,7 +27,45 @@
  | 12  |  C1   |  PB3   15 |     1      |
  +-----+-------+-----------+------------+
  
- This code was tested with NUM_DIGITS=4 and COMMON_PIN=COMMON_CATHODE
+ For 2*FJ5101BH (7-seg, common anode):
+ All segments are driven simultaneously (16 MCU outputs are used)
+ 
+ +-----+-------+-----------+------------+
+ | LED1| Descr |  MCU pin  | Level for  |
+ | pin |       | (ATMega8) | lighting   |
+ +-----+-------+-----------+------------+
+ | 1   |  e    |  PD4   2  |     0      |
+ | 2   |  d    |  PC3   26 |     0      |
+ | 3   | Anode |  +5V      |     1      |
+ | 4   |  c    |  PC2   25 |     0      |
+ | 5   |  dp   |  PD7   11 |     0      |
+ | 6   |  b    |  PC1   24 |     0      |
+ | 7   |  a    |  PC0   23 |     0      |
+ | 8   | Anode |  +5V      |     1      |
+ | 9   |  f    |  PD5   9  |     0      |
+ | 10  |  g    |  PD6   10 |     0      |
+ +-----+-------+-----------+------------+
+ +-----+-------+-----------+------------+
+ | LED2| Descr |  MCU pin  | Level for  |
+ | pin |       | (ATMega8) | lighting   |
+ +-----+-------+-----------+------------+
+ | 1   |  e    |  PB4   16 |     0      |
+ | 2   |  d    |  PB3   15 |     0      |
+ | 3   | Anode |  +5V      |     1      |
+ | 4   |  c    |  PB2   14 |     0      |
+ | 5   |  dp   |  PB7   8  |     0      | USES_CRYSTAL=0
+ | 5   |  dp   |  PC5   28 |     0      | USES_CRYSTAL=1
+ | 6   |  b    |  PB1   13 |     0      |
+ | 7   |  a    |  PB0   12 |     0      |
+ | 8   | Anode |  +5V      |     1      |
+ | 9   |  f    |  PB5   17 |     0      |
+ | 10  |  g    |  PB6   7  |     0      | USES_CRYSTAL=0
+ | 10  |  g    |  PC4   27 |     0      | USES_CRYSTAL=1
+ +-----+-------+-----------+------------+
+ 
+ This code was tested with:
+  NUM_DIGITS=4 and COMMON_PIN=COMMON_CATHODE
+  NUM_DIGITS=2 and COMMON_PIN=COMMON_ANODE and USES_CRYSTAL=0
  
  */ 
 
@@ -45,7 +83,7 @@
 // Set number of digits on your indicator. Can be 2 or 4
 // 4 is for SMA420564 and similar.
 // 2 is for 2*FJ5101BH and similar.
-#define NUM_DIGITS 4
+#define NUM_DIGITS 2
 
 // If 1-digit LEDs are installed there are two options. 
 // If you need crystal (i.e. Atmel's internal calibrated RC oscillator
